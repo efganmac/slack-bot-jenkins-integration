7c7299390b2bb2a65883e2d4cf8b2c3a1666e4b2
efekanbicermac <efekan.bicer8@gmail.com>
2023-11-14 22:42:21 +0300

    Add JSON processing logic

--- send-jenkins-notification/slack-notification.go
+++ send-jenkins-notification/slack-notification.go
@@ -1,50 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/slack-go/slack"
+	"net/http"
 	"os"
 )
 
-func main() {
-	args := os.Args[1:]
-	fmt.Println(args)
+type jenkinsBuild struct {
+	BuildURL    string `json:"buildurl"`
+	BuildResult string `json:"buildresult"`
+	BuildNumber int    `json:"buildnumber"`
+	JobName     string `json:"jobname"`
+}
 
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-2567467416019-6140180254420-H2HPpcBkLBPqVxiF1jiljSl8")
-	api := slack.New("xoxb-2567467416019-6140180254420-H2HPpcBkLBPqVxiF1jiljSl8")
-	preText := "*Hello, your Jenkins build FINISHED!*"
-	jenkinsURL := "*Build URL:*  " + args[0]
-	buildResult := "*" + args[1] + "*"
-	buildNumber := "*" + args[2] + "*"
-	jobName := "*" + args[3] + "*"
+func sendSlackMessage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Sent Slack Message!</h1>")
+	build := jenkinsBuild{}
+	err0 := json.NewDecoder(r.Body).Decode(&build)
+	if err0 != nil {
+		http.Error(w, err0.Error(), http.StatusBadRequest)
+		return
+	}
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	fmt.Println(build)
+	jenkinsURL := "*Build URL:* " + build.BuildURL
+	buildResult := "*" + build.BuildResult + "*"
+	buildNumber := "*" + fmt.Sprint(build.BuildNumber) + "*"
+	jobName := "*" + build.JobName + "*"
 
 	if buildResult == "*SUCCESS*" {
 		buildResult = buildResult + " :white_check_mark:"
 	} else {
-		buildResult = buildResult + ":x:"
+		buildResult = buildResult + " :x:"
 	}
 
+	preText := "*Hello! Your Jenkins build has finished!*"
 	dividerSection1 := slack.NewDividerBlock()
-	jenkinsBuildDetails := jobName + "#" + buildNumber + "-" + buildResult + "\n" + jenkinsURL
+	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
 	preTextField := slack.NewTextBlockObject("mrkdwn", preText+"\n\n", false, false)
 	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
 
-	jenkinsBuildDetailSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
+	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
 	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
 
 	msg := slack.MsgOptionBlocks(
 		preTextSection,
 		dividerSection1,
-		jenkinsBuildDetailSection,
+		jenkinsBuildDetailsSection,
 	)
 
 	_, _, _, err := api.SendMessage(
 		"C02HD32CVRN",
 		msg,
 	)
+
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return
 	}
+}
+
+func main() {
+
+	http.HandleFunc("/sendSlackMessage", sendSlackMessage)
+	http.ListenAndServe(":8091", nil)
 
 }
